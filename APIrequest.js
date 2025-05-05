c5ed5f984452c251663dbb82eaba438e3c306189
Priyanshu Pandey <ppriyanshu.p2005@gmail.com>
2025-05-05 13:23:23 +0530

    Second commit

--- APIrequest.js
+++ APIrequest.js
@@ -24,4 +24,41 @@ const putData=(req,res)=>{
 
 app.listen(3000,(e)=>console.log('server is listning at port http://localHost:3000'));
 
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product'); 
+
+router.post('/add-product', async (req, res) => {
+  const { name, price, discount, category } = req.body;
+
+  try {
+    
+    const newProduct = new Product({
+      name,
+      price,
+      discount,
+      category,
+    });
+
+  
+    await newProduct.save();
+
+    res.status(201).json({ message: 'Product added successfully', product: newProduct });
+  } catch (error) {
+    res.status(500).json({ message: 'Error adding product', error: error.message });
+  }
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
 
